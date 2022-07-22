575519af1b10ec1b9e2af07158544cf772507077
Fernando Pascual Soriano <fer112233mc@gmail.com>
2022-07-22 09:46:29 +0200

    feat: NFT modifications block + Indexer synchronization

--- api/nfts/models/nfts.js
+++ api/nfts/models/nfts.js
@@ -5,6 +5,10 @@
  * to customize this model
  */
 
+const {algoIndexer} = require("../../../config/algorand");
+const algosdk = require("algosdk");
+
+
 module.exports = {
   lifecycles: {
     // Called before an entry is created
@@ -12,5 +16,23 @@ module.exports = {
       data.burnWillTimeoutOn = Date.now()
       data.supply_remaining = data.supply
     },
+    beforeUpdate: async function (params, newNFT) {
+      const { _id } = params
+      const oldNft = await strapi.services.nfts.findOne({ _id })
+      if (oldNft.id !== _id) throw new Error(`NFT not found with id: ${_id}`)
+      for (const key of Object.keys(newNFT)) {
+        if (["status", "burnWillTimeoutOn"].includes(key)) continue;
+        if (key === "supply_remaining") {
+          const indexerClient = algoIndexer()
+          const NFTHoldings = await indexerClient.lookupAssetBalances(Number(oldNft.asa_id)).do()
+          const vaultAddress = algosdk.getApplicationAddress(Number(process.env.APP_ID))
+          const vaultHoldings = NFTHoldings.balances.filter(accountHoldings => accountHoldings.address === vaultAddress)[0]
+          if (vaultHoldings.amount !== Number(newNFT[key])) console.warn("Attempting to update supply_remaining with incorrect value according to the indexer. Using indexer data instead.")
+          newNFT[key] = vaultHoldings.amount
+          continue
+        }
+        newNFT[key] = oldNft[key]
+      }
+    }
   },
 }
